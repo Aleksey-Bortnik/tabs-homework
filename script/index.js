7df499862b3b8832f06c9216ba5746690d5674d6
Aleksey <Aleksey.bortnik91@gmail.com>
2020-11-17 05:34:00 +0300

    article is ready.

---
+++ script/index.js
@@ -0,0 +1,63 @@
+const tabBtnIdPrefix = "tab-btn-";
+const tabContentIdPrefix = "tab-content-";
+const activeClass = "active";
+
+function hideActiveTab() {
+  let $activeElements = document.querySelectorAll("." + activeClass);
+  for (let $activeElement of $activeElements) {
+    $activeElement.classList.remove(activeClass);
+  }
+}
+
+function onTabClicked(event) {
+  hideActiveTab();
+
+  let target = event.target;
+
+  target.classList.add(activeClass);
+
+  let targetNumber = target.id.split("-")[2];
+  let activeTabId = tabContentIdPrefix + targetNumber;
+
+  let $activeTab = document.getElementById(activeTabId);
+  $activeTab.classList.add("active");
+}
+
+function onTabKeyPressed(event) {
+  if (event.keyCode !== 9) {
+    return;
+  }
+
+  const $activeTabBtn = document.querySelector(".tab-button.active");
+
+  const currentActiveTabNumber = +$activeTabBtn.id.split("-")[2];
+
+  const numberOfTabs = document.querySelectorAll(".tab-button").length;
+
+  const isActiveTabLast = currentActiveTabNumber == numberOfTabs;
+
+  let newActiveTabNumber;
+  if (isActiveTabLast) {
+    newActiveTabNumber = 1;
+  } else {
+    newActiveTabNumber = currentActiveTabNumber + 1;
+  }
+
+  const tabBtnId = tabBtnIdPrefix + newActiveTabNumber;
+  const tabContentId = tabContentIdPrefix + newActiveTabNumber;
+
+  hideActiveTab();
+
+  document.getElementById(tabBtnId).classList.add(activeClass);
+  document.getElementById(tabContentId).classList.add(activeClass);
+
+  event.preventDefault();
+}
+
+let $allTabButtons = document.querySelectorAll(".tab-button");
+
+for (let $tabButton of $allTabButtons) {
+  $tabButton.addEventListener("click", onTabClicked);
+}
+
+document.addEventListener("keydown", onTabKeyPressed);
